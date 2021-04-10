191f204f0ed58ef64edd3fdba89b4dc3637fa85e
NITESH_PANT <54784082+Niteshpant2000@users.noreply.github.com>
2021-04-10 11:48:44 +0530

    Update FruitCountReducer.java

--- FruitCountReducer.java
+++ FruitCountReducer.java
@@ -6,7 +6,7 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.Reducer.Context;
 
-public class WordCountReducer extends Reducer<Text, IntWritable, Text, IntWritable>{
+public class FruitCountReducer extends Reducer<Text, IntWritable, Text, IntWritable>{
 
 	@Override
 	public void reduce(Text key, Iterable<IntWritable> values, Context context)
