8ff9c0092f10d649bc3d17e118edb6567ae8b71a
NITESH_PANT <54784082+Niteshpant2000@users.noreply.github.com>
2021-04-10 11:45:14 +0530

    Create WordCountMapper.java

---
+++ WordCountMapper.java
@@ -0,0 +1,34 @@
+
+
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class WordCountMapper extends Mapper<LongWritable,  Text, Text, IntWritable>
+
+{
+
+	@Override
+	protected void map(LongWritable key, Text value,
+			org.apache.hadoop.mapreduce.Mapper.Context context)
+			throws IOException, InterruptedException {
+String inputstring = value.toString();
+
+for(String fruit: inputstring.split(" "))
+
+{	fruit=fruit.toLowerCase();
+	if(fruit.equals("apple") || fruit.equals("banana") || fruit.equals("grapes")){
+		context.write(new Text(fruit),new IntWritable(1));
+		
+	}
+
+}
+	}
+
+}
