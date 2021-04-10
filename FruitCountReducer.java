dd683ddbb8f0a8f4b1c7f92acc3fe8705d364c28
NITESH_PANT <54784082+Niteshpant2000@users.noreply.github.com>
2021-04-10 11:46:42 +0530

    Create FruitCountReducer.java

---
+++ FruitCountReducer.java
@@ -0,0 +1,25 @@
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.Reducer.Context;
+
+public class WordCountReducer extends Reducer<Text, IntWritable, Text, IntWritable>{
+
+	@Override
+	public void reduce(Text key, Iterable<IntWritable> values, Context context)
+	throws IOException, InterruptedException {
+		int fruitval = 0;
+		for(IntWritable fruit : values)
+		{
+			fruitval ++;
+			
+		}
+		context.write(key, new IntWritable(fruitval) );
+		
+	}
+
+}
+
